690a650fb680dcf011d64664e2d7cc718dcd4052
degraw <nathan.degraw@gmail.com>
2013-03-20 21:17:39 -0400

    Cleaned up code

--- src/main/java/hw7/HelloDatabaseWorldServlet.java
+++ src/main/java/hw7/HelloDatabaseWorldServlet.java
@@ -1,75 +1,35 @@
 package hw7;
 
-import java.io.*;
+import java.io.IOException;
 
-import javax.servlet.*;
-import javax.servlet.annotation.*;
-import javax.servlet.http.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.HttpRequestHandler;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
+
 
 //@WebServlet("/HelloDatabaseWorld")
 //@WebServlet(description = "Http Servlet using pure java / annotations", urlPatterns = { "/HelloDatabaseWorld" }, name = "HelloDatabaseWorldServlet")
 public class HelloDatabaseWorldServlet implements HttpRequestHandler {
-//public class HelloDatabaseWorldServlet extends HttpServlet implements HttpRequestHandler {
-	private static final long serialVersionUID = 5369735834465318258L;
-	
-	KeyValueDatabase database=null;
-	
-	
+
+	KeyValueDatabase database = null;
 
 	public void setDatabase(KeyValueDatabase database) {
 		this.database = database;
 	}
 
-//	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	@Override
+	public void handleRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String bgColor = request.getParameter("bgColor");
-		String fgColor = request.getParameter("fgColor");
+		database.actionWriter();
+		database.actionReader();
 
-		if (bgColor != null && fgColor != null
-				&& bgColor.trim().equals(fgColor.trim())) {
-			bgColor = "";
-			fgColor = "";
-		}
+		request.setAttribute("databaseMessage", database.getDbMessage());
 
-		ColorBeanImmutable elColor = new ColorBeanImmutable(bgColor, fgColor);
-		request.setAttribute("elColor", elColor);
-		
-		String databaseMessage;
-		
-		try {
-			if (database == null) {
-			//WebApplicationContext ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(this.getServletContext());
-			 //database = (KeyValueDatabase) ctx.getBean("KeyValueDatabase");
-			}
-			
-			
-			database.actionWriter();
-			database.actionReader();
-
-			databaseMessage =  database.getDbMessage();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			// e.printStackTrace();
-			
-			databaseMessage = new String("The Database is being worked on now, try agian later.");
-		}
-		request.setAttribute("databaseMessage", databaseMessage);
-		
 		String address = "HelloDatabaseWorld.jsp";
 		RequestDispatcher dispatcher = request.getRequestDispatcher(address);
-		dispatcher.forward(request, response);
-	}
-
-	@Override
-	public void handleRequest(HttpServletRequest arg0, HttpServletResponse arg1)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(arg0, arg1);
+		dispatcher.forward(request, response);;
 	}
 }
